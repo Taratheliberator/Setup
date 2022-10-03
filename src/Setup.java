2535fbc41f45bc436a4ceedb7fbe1479966e02b0
Viner Gindullin <Taratheliberator@gmail.com>
2022-10-03 16:28:54 +0500

    Упаковал в методы, коллекцию файлов сделал строковыми

--- src/Setup.java
+++ src/Setup.java
@@ -1,52 +1,71 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Setup {
+    static StringBuilder sb = new StringBuilder();
 
-        public static void main(String[] args) {
-            StringBuilder sb = new StringBuilder();
-            List<File> folderList = Arrays.asList(
-                    new File("C://Games"),
-                    new File("C://Games//temp"),
-                    new File("C://Games//src"),
-                    new File("C://Games//res"),
-                    new File("C://Games//saveGames"),
-                    new File("C://Games//src//main"),
-                    new File("C://Games//src//test"),
-                    new File("C://Games//res//drawables"),
-                    new File("C://Games//res//vectors"),
-                    new File("C://Games//res//icons")
-            );
-            List<File> fileList = Arrays.asList(
-                    new File("C://Games//src//main//Main.java"),
-                    new File("C://Games//src//main//Utils.java"),
-                    new File("C://Games//temp//temp.txt")
-            );
-            folderList.forEach(folder -> {
-                if (folder.mkdir()) sb.append("Каталог ").append(folder).append(" создан\n");
-                else sb.append("Каталог ").append(folder).append(" не создан\n");
-            });
-            fileList.forEach(file -> {
-                try {
-                    if (file.createNewFile()) sb.append("Файл ").append(file).append(" создан\n");
-                    else sb.append("Файл ").append(file).append(" не создан\n");
-                } catch (IOException ex) {
-                    sb.append(ex.getMessage()).append('\n');
-                }
-            });
-            try (FileWriter log = new FileWriter("C://Games//temp//temp.txt", false)) {
-                log.write(sb.toString());
-                log.flush();
+    public static void main(String[] args) {
+
+        List<String> folderList = Arrays.asList(
+                "C://Games",
+                "C://Games//temp",
+                "C://Games//src",
+                "C://Games//res",
+                "C://Games//saveGames",
+                "C://Games//src//main",
+                "C://Games//src//test",
+                "C://Games//res//drawables",
+                "C://Games//res//vectors",
+                "C://Games//res//icons"
+        );
+        List<String> fileList = Arrays.asList(
+                "C://Games//src//main//Main.java",
+                "C://Games//src//main//Utils.java",
+                "C://Games//temp//temp.txt"
+        );
+        String log = "C://Games//temp//temp.txt";
+        makeDirectory(folderList);
+        makeFile(fileList);
+        writeToFile(log);
+        readFromFileToConsole(log);
+
+    }
+
+    public static void makeFile(List<String> fileList) {
+        fileList.forEach(file -> {
+            try {
+                if (new File(file).createNewFile()) sb.append("The file ").append(file).append(" is created\n");
+                else sb.append("The file ").append(file).append(" is not created\n");
             } catch (IOException ex) {
                 sb.append(ex.getMessage()).append('\n');
             }
-            try (BufferedReader br = new BufferedReader(new FileReader("C://Games//temp//temp.txt"))) {
-                String s;
-                while ((s = br.readLine()) != null) System.out.println(s);
-            } catch (IOException ex) {
-                System.out.println(ex.getMessage());
-            }
+        });
+    }
+
+    public static void makeDirectory(List<String> folderList) {
+        folderList.forEach(folder -> {
+            if (new File(folder).mkdir()) sb.append("The folder ").append(folder).append(" is created\n");
+            else sb.append("The folder ").append(folder).append(" is not created\n");
+        });
+    }
+
+    public static void writeToFile(String path) {
+        try (FileWriter log = new FileWriter(path, false)) {
+            log.write(sb.toString());
+            log.flush();
+        } catch (IOException ex) {
+            sb.append(ex.getMessage()).append('\n');
         }
     }
+    public static void readFromFileToConsole(String path) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String s;
+            while ((s = br.readLine()) != null) System.out.println(s);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
 
